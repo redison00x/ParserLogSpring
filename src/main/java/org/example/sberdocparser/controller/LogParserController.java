af679a2fdacd5d0564e2f2c57228091a1af7dd6c
redison00x <praimebotob@gmail.com>
2025-04-28 15:41:20 +0300

    3comm

--- src/main/java/org/example/sberdocparser/controller/LogParserController.java
+++ src/main/java/org/example/sberdocparser/controller/LogParserController.java
@@ -3,6 +3,8 @@ package org.example.sberdocparser.controller;
 import org.example.sberdocparser.model.LogEntry;
 import org.example.sberdocparser.service.LogParserService;
 import org.example.sberdocparser.service.LogWriterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @Controller
 public class LogParserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogParserController.class);
 
     @Autowired
     private LogParserService logParserService;
@@ -36,19 +40,38 @@ public class LogParserController {
             model.addAttribute("message", "Выбери файл для загрузки");
             return "status";
         }
+
+        File tempFile = null;
         try {
-            File tempFile = File.createTempFile("log", ".log");
+            tempFile = File.createTempFile("log", ".log");
+            logger.info("Временный файл создан: " + tempFile.getAbsolutePath());
+
             file.transferTo(tempFile);
+            logger.info("Размер временного файла: " + tempFile.length() + " байт");
+
             String outputFilePath = "output.txt";
             List<LogEntry> logEntries = logParserService.parseLogFile(tempFile.getAbsolutePath());
+            logger.info("Найдено log entries: " + logEntries.size());
+
             logWriterService.writeLogToFile(logEntries, outputFilePath);
+            logger.info("Файл записан: " + outputFilePath);
 
             model.addAttribute("message", "Файл распарсен в output.txt");
             return "status";
 
         } catch (IOException e) {
+            logger.error("Ошибка парсинга файла: " + e.getMessage(), e);
             model.addAttribute("message", "Ошибка парсинга файла: " + e.getMessage());
             return "status";
+
+        } finally {
+            if (tempFile != null) {
+                if(tempFile.delete()) {
+                    logger.info("Временный файл удален: " + tempFile.getAbsolutePath());
+                } else {
+                    logger.error("Не удалось удалить временный файл: " + tempFile.getAbsolutePath());
+                }
+            }
         }
     }
 }
--- src/main/java/org/example/sberdocparser/service/LogParserService.java
+++ src/main/java/org/example/sberdocparser/service/LogParserService.java
@@ -1,7 +1,10 @@
 package org.example.sberdocparser.service;
 
 import org.example.sberdocparser.model.LogEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,11 +14,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
 public class LogParserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogParserService.class);
 
     public List<LogEntry> parseLogFile(String fileName) {
         List<LogEntry> logEntries = new ArrayList<LogEntry>();
@@ -37,14 +43,17 @@ public class LogParserService {
                         logEntry.setCommand(matcher.group(2));
                         logEntry.setAmount(Double.parseDouble(matcher.group(3)));
                         logEntries.add(logEntry);
+                        logger.debug("Найдено Command: " + logEntry);
+
                     } catch (ParseException e) {
-                        System.out.println("Ошибка парсинга: " + e.getMessage());
-                    } }
-                else if (resultMatcher.find()) {
+                        logger.error("Ошибка парсинга даты: "+e.getMessage(),e);
+                    }
+                } else if (resultMatcher.find()) {
                         if (!logEntries.isEmpty()){
                             LogEntry lastEntry = logEntries.get(logEntries.size()-1);
                             lastEntry.setPaymentStatus("Успешно");
                             lastEntry.setCardEnding(resultMatcher.group(2));
+                            logger.debug("Найдено Result: " + lastEntry);
                         }
                 }
             }
