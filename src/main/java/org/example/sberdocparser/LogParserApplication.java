4ff795500ec23579885358dbd95e0c5ea77cf85f
redison00x <praimebotob@gmail.com>
2025-04-30 16:28:05 +0300

    4comm проблема с логами остаётся

--- src/main/java/org/example/sberdocparser/LogParserApplication.java
+++ src/main/java/org/example/sberdocparser/LogParserApplication.java
@@ -1,5 +1,7 @@
 package org.example.sberdocparser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,8 +9,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LogParserApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(LogParserApplication.class);
+
     public static void main(String[] args) {
 
+        logger.debug("This is a DEBUG message from LogParserApplication!");
         SpringApplication.run(LogParserApplication.class, args);
     }
 
